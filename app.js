032c012f43ad851f58bf3c9039dfe366816182db
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-25 17:01:12 -0700

    setting up more

--- app.js
+++ app.js
@@ -4,11 +4,23 @@
 var express = require('express');
 var app = express();
 
+app.set("view engine","ejs");
+
+var campGrounds = [];
 
 app.get("/", function (req,res) {
-   res.send("this will be the landing page soon");
+   res.render("landing");
 });
 
+app.get("/campgrounds", function (req,res) {
+campGrounds = [{name:"Rest", image:"https://images.pexels.com/photos/48638/pexels-photo-48638.jpeg?h=350&auto=compress&cs=tinysrgb"},
+    {name:"Play Time", image:"https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"},
+    {name:"Cat's Easy Dive", image:"https://unsplash.com/search/camp?photo=B9z9TjfIw3I"},
+    {name:"Take it and Go", image:"https://unsplash.com/search/camp?photo=rRljZzjNQAA"}];
+
+    res.render("campgrounds")
+
+});
 
 app.listen(3000, function () {
     console.log('Yelp Camp listening on port 3000!')
