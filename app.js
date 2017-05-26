71d2ce111096e51cfc0426bb93dd655d0f847857
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-25 17:18:42 -0700

    Added images from web

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.get("/", function (req,res) {
 app.get("/campgrounds", function (req,res) {
 campGrounds = [{name:"Rest", image:"https://images.pexels.com/photos/48638/pexels-photo-48638.jpeg?h=350&auto=compress&cs=tinysrgb"},
     {name:"Play Time", image:"https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"},
-    {name:"Cat's Easy Dive", image:"https://unsplash.com/search/camp?photo=B9z9TjfIw3I"},
-    {name:"Take it and Go", image:"https://unsplash.com/search/camp?photo=rRljZzjNQAA"}];
+    {name:"Cat's Easy Dive", image:"https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&auto=compress&cs=tinysrgb"},
+    {name:"Take it and Go", image:"https://images.pexels.com/photos/266751/pexels-photo-266751.jpeg?h=350&auto=compress&cs=tinysrgb"}];
 
-    res.render("campgrounds")
+    res.render("campgrounds",{campgrounds:campGrounds})
 
 });
 
