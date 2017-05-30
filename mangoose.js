7a32d81ba3b87f0396273e4a5f9a8a00ba4505e7
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-29 22:38:34 -0700

    Added search from Mongoose ODM

--- mangoose.js
+++ mangoose.js
@@ -8,16 +8,25 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+//
+// var george = new Cat({
+//     name:"George Williams",
+//     age : 9,
+//     temperament:"Excited Cat"
+// });
+//
+// george.save(function (err) {
+//    if (err)
+//        console.log("Couldn't save george: "+ err);
+//    else
+//        console.log("George is saved.");
+// });
 
-var george = new Cat({
-    name:"George",
-    age : 5,
-    temperament:"Happy Cat"
-});
 
-george.save(function (err) {
-   if (err)
-       console.log("Couldn't save george: "+ err);
-   else
-       console.log("George is saved.");
+Cat.find({name:"George"}, function (err,cats) {
+    if (err)
+           console.log("Error!");
+    else
+        console.log("All");
+        console.log(cats);
 });
