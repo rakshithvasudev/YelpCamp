a477ee4277442e71ec4a58c54dba0e9388a8e703
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-29 22:30:34 -0700

    added mangoose

--- mangoose.js
+++ mangoose.js
@@ -1,1 +1,23 @@
 var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/mongoose_app");
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+var george = new Cat({
+    name:"George",
+    age : 5,
+    temperament:"Happy Cat"
+});
+
+george.save(function (err) {
+   if (err)
+       console.log("Couldn't save george: "+ err);
+   else
+       console.log("George is saved.");
+});
